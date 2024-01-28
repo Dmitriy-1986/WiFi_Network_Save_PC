fa27fbe037175b414cf02a7158b63a36be29582b
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2024-01-28 23:27:39 +0200

    Add files via upload

---
+++ pass_wifi.py
@@ -0,0 +1,25 @@
+import subprocess
+
+try:
+    data = subprocess.check_output("netsh wlan show profiles").decode('cp866').split('\n')
+    profiles = [i.split(":")[1].strip() for i in data if "Все профили пользователей" in i]
+    pass_wifi = ""
+
+    for i in profiles:
+        if i.strip():
+            try:
+                results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('cp866').split('\n')
+                for j in results:
+                    if "Содержимое ключа" in j:
+                        password = j.split(':')[1].strip()
+                        pass_wifi += f"--------------------------------\n\nНазвание сети: {i} \nПароль: {password}\n\n"
+            except subprocess.CalledProcessError as e:
+                print(f"Ошибка при получении пароля для профиля {i}: {e}")
+
+    if not pass_wifi:
+        print("Нет сохраненных паролей WiFi.")
+    else:
+        print(pass_wifi)
+
+except Exception as ex:
+    print(f'Ошибка: {ex}')
